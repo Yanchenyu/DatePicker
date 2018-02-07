ca468512f379edfb4a839198da4111a0d3b4c5e3
yanchenyu <chenyu.yan@insevo.cn>
2018-02-07 11:25:39 +0800

    完成部分样式修改以及选中日期修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,5 @@
 var gulp = require("gulp"),
-    babel = require("gulp-babel"),
     connect = require("gulp-connect");
-
-gulp.task("readJs", function () {
-  return gulp.src("./js/index.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist/js"));
-});
 
 // var watcher = gulp.watch('index.js', ['default']);
 // watcher.on('change', function(event) {
@@ -28,9 +21,9 @@ gulp.task('html', function() {
 
 gulp.task('watch', function() {
     gulp.watch('./css/*.css', ['html']); //监控css文件
-    gulp.watch('./js/*.js', ['readJs', 'html']); //监控js文件
+    gulp.watch('./js/*.js', ['html']); //监控js文件
     gulp.watch(['./*.html'], ['html']); //监控html文件
 }); //执行gulp server开启服务器
 
 
-gulp.task('default', ['readJs', 'connect', 'watch']);
+gulp.task('default', ['connect', 'watch']);
--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 window.onload = function(){
 
     // 默认是当天
-    let chosenDate = new Date();
-    let year = chosenDate.getFullYear();
-    let month = chosenDate.getMonth();
-    let date = chosenDate.getDate();
+    var chosenDate = new Date(),
+        year = chosenDate.getFullYear(),
+        month = chosenDate.getMonth(),
+        date = chosenDate.getDate();
 
-    const render = (year, month, date = 1) => {
+    var getIdDom = function(id){
+        return document.getElementById(id)
+    };  // 根据id获取dom节点
+
+    var render = (year, month, date = 1) => {
         month++;
-        const datePage = [];    // 最终展示页面的所有日期合集        
+        var datePage = [];    // 最终展示页面的所有日期合集        
         // 第一部分，上月月末几天
         // 首先要知道上月一共多少天
-        const getAlldays = (year, month) => {
+        var getAlldays = (year, month) => {
             if (month <= 7) {
                 // 1-7月
                 if (month % 2 === 0) {
@@ -42,58 +46,109 @@ window.onload = function(){
             }
             return alldays
         };      // alldays表示某年某月的总天数
-        let lastMonthAllDays = getAlldays(year, month - 1);    // 上月天数
-        let chosenFirstMonthDay = new Date(year, month - 1, 1).getDay();    // 当月1号周几
+        var lastMonthAllDays = getAlldays(year, month - 1);    // 上月天数
+        var chosenFirstMonthDay = new Date(year, month - 1, 1).getDay();    // 当月1号周几
 
         var datePageTemplate = ``;
         var num = 0;
 
-        for (let i = lastMonthAllDays - chosenFirstMonthDay + 1; i < lastMonthAllDays + 1; i++ ) {
-            datePageTemplate += `<span>${i}</span>`;
+        for (var i = lastMonthAllDays - chosenFirstMonthDay + 1; i < lastMonthAllDays + 1; i++ ) {
+            datePageTemplate += `<span id="lastMonth"><em id="last-${i}" onclick="chooseDate(this, 'last-${i}', ${year}, ${month}, ${i})">${i}</em></span>`;
             num++;
         }
 
         // 第二部分，当月总天数
-        let chosenMonthAllDays = getAlldays(year, month);  // 当月天数
-        for(let i = 1; i < chosenMonthAllDays + 1; i++) {
-            datePageTemplate += `<span class="currentMonth">${i}</span>`;
+        var chosenMonthAllDays = getAlldays(year, month);  // 当月天数
+        var time = new Date();
+        var a = time.getFullYear(),
+            b = time.getMonth() + 1,
+            c = time.getDate(); // 用来记录当天时间
+        for(var i = 1; i < chosenMonthAllDays + 1; i++) {
+            var chosenDateObj = {
+                year: year,
+                month: month,
+                date: i
+            };
+            if (i === c && year === a && month === b) {
+                // 今天日期高亮
+                datePageTemplate += `<span id="today" class="currentMonth"><em id="today-${i}" onclick="chooseDate(this, 'today-${i}', ${year}, ${month}, ${i})" class="today">${i}</em></span>`;
+            } else {
+                datePageTemplate += `<span id="currentMonth" class="currentMonth"><em id="cur-${i}" onclick="chooseDate(this, 'cur-${i}', ${year}, ${month}, ${i})">${i}</em></span>`;
+            }
+            
             num++;
         }
 
         // 第三部分，剩余天数
-        for (let i = 1; i < 43 - num; i++) {
-            datePageTemplate += `<span>${i}</span>`;
+        for (var i = 1; i < 43 - num; i++) {
+            var chosenDateObj = {
+                year: year,
+                month: month,
+                date: i
+            };
+            datePageTemplate += `<span id="nextMonth"><em id="nex-${i}" onclick="chooseDate(this, 'nex-${i}', ${year}, ${month}, ${i})">${i}</em></span>`;
         }
 
 
-        let templateString = `${datePageTemplate}`;
+        var templateString = `${datePageTemplate}`;
 
-        document.getElementById('dateYear').innerHTML = year;
-        document.getElementById('dateMonth').innerHTML = month;
-        document.getElementById('dateBody').innerHTML = templateString;
+        var innerFn = function(id, content) {
+            getIdDom(id).innerHTML = content
+        };
+        innerFn('dateYear', year);
+        innerFn('dateMonth', month);
+        innerFn('dateBody', templateString);
     
     }
 
     render(year, month, date);
 
-    document.getElementById('yearLeft').onclick = () => {
-        year--;
-        render(year, month, date)
-    }
+    var clickFn = function(id, fn) {
+        getIdDom(id).onclick = fn;
+    };  // 封装一下click方法
 
-    document.getElementById('monthLeft').onclick = () => {
+    // 上一年下一年，上一月下一月的点击事件
+    clickFn('yearLeft', function(){
+        year--;
+        render(year, month, date);
+    });
+
+    clickFn('monthLeft', function(){
         month--;
         render(year, month, date)
-    }
+    });
 
-    document.getElementById('yearRight').onclick = () => {
+    clickFn('yearRight', function(){
         year++;
         render(year, month, date)
-    }
+    });
 
-    document.getElementById('monthRight').onclick = () => {
+    clickFn('monthRight', function(){
         month++;
         render(year, month, date)
-    }
+    });
 
+    // input框获取焦点时日历显示
+
+    var datePicker = getIdDom('datePicker');
+
+    getIdDom('textInput').onfocus = function(){
+        datePicker.className = 'datePicker datePickerShow';
+    }
+    
+
+}
+
+var lastId = '';    // 挂到全局下去
+
+function chooseDate(item, index, year, month, date) {
+    event.stopPropagation();
+    if (lastId !== '') {
+        // 非第一次点击
+        document.getElementById(lastId).style = "";
+    }
+    // 选中项样式改变，并且将input的日期修改
+    lastId = index;
+    item.style = "background-color: #39f;color: #fff";
+    document.getElementById("textInput").value = year + '-' + month + '-' + date;
 }
